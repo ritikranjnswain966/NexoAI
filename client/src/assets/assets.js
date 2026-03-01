0834e8d7d7bec6ebace171f41ccc3d1d91df9e7a
ritik ranjn swain <ritikranjnswain966@gmail.com>
2026-03-01 07:45:43 +0530

    ..

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -6,7 +6,6 @@ import user_icon from "./user_icon.svg";
 import theme_icon from "./theme_icon.svg";
 import send_icon from "./send_icon.svg";
 import stop_icon from "./stop_icon.svg";
-import mountain_img from "./mountain_img.jpg";
 import menu_icon from "./menu_icon.svg";
 import close_icon from "./close_icon.svg";
 import bin_icon from "./bin_icon.svg";
@@ -14,23 +13,19 @@ import logout_icon from "./logout_icon.svg";
 import diamond_icon from "./diamond_icon.svg";
 import gallery_icon from "./gallery_icon.svg";
 
-
-
 export const assets = {
-    logo,
-    logo_full,
-    search_icon,
-    user_icon,
-    theme_icon,
-    send_icon,
-    stop_icon,
-    mountain_img,
-    menu_icon,
-    close_icon,
-    bin_icon,
-    logout_icon,
-    logo_full_dark,
-    diamond_icon,
-    gallery_icon
+  logo,
+  logo_full,
+  search_icon,
+  user_icon,
+  theme_icon,
+  send_icon,
+  stop_icon,
+  menu_icon,
+  close_icon,
+  bin_icon,
+  logout_icon,
+  logo_full_dark,
+  diamond_icon,
+  gallery_icon,
 };
-
